0b85efadc6fc23b48d34cfe87f0f59957d286c61
xbluesyan <40620374+xbluesyan@users.noreply.github.com>
2020-06-08 04:39:34 +0900

    3장 끝

--- src/main/java/com/syan/springboot/Application.java
+++ src/main/java/com/syan/springboot/Application.java
@@ -2,10 +2,14 @@ package com.syan.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    // insert into posts ( author, content, title ) values ('author', 'content', 'title');
 }
---
+++ src/main/java/com/syan/springboot/web/PostsApiController.java
@@ -0,0 +1,34 @@
+package com.syan.springboot.web;
+
+import com.syan.springboot.service.posts.PostsService;
+import com.syan.springboot.web.dto.PostsResponseDto;
+import com.syan.springboot.web.dto.PostsSaveRequestDto;
+
+import com.syan.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+}
